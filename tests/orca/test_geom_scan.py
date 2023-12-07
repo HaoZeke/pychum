0d9444a843c28053684fbaec764e1102cf74272f
Rohit Goswami <rog32@hi.is>
2023-12-07 21:31:19 +0000

    TST: Rework for full config

--- tests/orca/test_geom_scan.py
+++ tests/orca/test_geom_scan.py
@@ -1,34 +1,34 @@
 import pytest
-from pychum.engine.orca._dataclasses import GeomScan, OrcaGeom
+from pychum.engine.orca._dataclasses import GeomScan, GeomBlock
 from pychum.engine.orca._renderer import OrcaInputRenderer
 
 # Helper function to render the geometry scan block
 def render_geom_scan(orca_geom):
-    config = {'orca_geom': orca_geom}
+    config = {'blocks': {'geom': orca_geom}}
     renderer = OrcaInputRenderer(config)
-    return renderer.render('geom_scan.jinja')
+    return renderer.render('geom.jinja')
 
 # Test cases
 def test_single_bond_scan():
-    geom = OrcaGeom(bonds=[GeomScan(atoms=[0, 1], range=[1.0, 2.0], points=10)])
+    geom = GeomBlock(bonds=[GeomScan(atoms=[0, 1], range=[1.0, 2.0], points=10)])
     result = render_geom_scan(geom)
-    expected = "%geom\n Scan\n    B 0 1 = 1.0, 2.0, 10\n end\nend"
+    expected = "%geom \n Scan\n    B 0 1 = 1.0, 2.0, 10\n end\nend"
     assert result.strip() == expected
 
 def test_single_angle_scan():
-    geom = OrcaGeom(angles=[GeomScan(atoms=[0, 1, 2], range=[90, 180], points=5)])
+    geom = GeomBlock(angles=[GeomScan(atoms=[0, 1, 2], range=[90, 180], points=5)])
     result = render_geom_scan(geom)
-    expected = "%geom\n Scan\n    A 0 1 2 = 90, 180, 5\n end\nend"
+    expected = "%geom \n Scan\n    A 0 1 2 = 90, 180, 5\n end\nend"
     assert result.strip() == expected
 
 def test_single_dihedral_scan():
-    geom = OrcaGeom(dihedrals=[GeomScan(atoms=[0, 1, 2, 3], range=[0, 360], points=20)])
+    geom = GeomBlock(dihedrals=[GeomScan(atoms=[0, 1, 2, 3], range=[0, 360], points=20)])
     result = render_geom_scan(geom)
-    expected = "%geom\n Scan\n    D 0 1 2 3 = 0, 360, 20\n end\nend"
+    expected = "%geom \n Scan\n    D 0 1 2 3 = 0, 360, 20\n end\nend"
     assert result.strip() == expected
 
 def test_combined_scans():
-    geom = OrcaGeom(
+    geom = GeomBlock(
         bonds=[GeomScan(atoms=[0, 1], range=[1.0, 2.0], points=10)],
         angles=[GeomScan(atoms=[0, 1, 2], range=[90, 180], points=5)],
         dihedrals=[GeomScan(atoms=[0, 1, 2, 3], range=[0, 360], points=20)]
@@ -36,7 +36,7 @@ def test_combined_scans():
     result = render_geom_scan(geom)
     # TODO(rg): Maybe fix the spacing here?
     expected = (
-        "%geom\n"
+        "%geom \n"
         " Scan\n"
         "    B 0 1 = 1.0, 2.0, 10\n\n"
         "    A 0 1 2 = 90, 180, 5\n\n"
@@ -47,7 +47,7 @@ def test_combined_scans():
     assert result.strip() == expected
 
 def test_exceeding_scan_limit():
-    geom = OrcaGeom(
+    geom = GeomBlock(
         bonds=[GeomScan(atoms=[0, 1], range=[1.0, 2.0], points=10)] * 4
     )
     result = render_geom_scan(geom)
